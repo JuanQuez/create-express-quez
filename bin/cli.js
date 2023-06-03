7dc7f86343537d112e5ff4ce0d40facce79f7420
JuanQuez <fiestalatina.ft@gmail.com>
2023-06-03 14:50:18 -0500

    template | express v3

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-const {execSync} = require('child_process');
+const { execSync } = require('child_process');
 
 const runCommand = command => {
     try {
-        execSync(`${command}`, {stdio: `inherit`});
+        execSync(`${command}`, { stdio: `inherit` });
     } catch (e) {
         console.error(`Failed to execute ${command}`, e);
         return false;
@@ -12,18 +12,17 @@ const runCommand = command => {
     return true;
 }
 
-
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/JuanQuez/create-express-quez ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
-console.log(`cloning the repository with name ${repoName}`);
+console.log(`🤖 Installing your project with the name ${repoName} 🤖`);
 const checkedOut = runCommand(gitCheckoutCommand);
-if(!checkedOut) process.exit(code, -1);
+if (!checkedOut) process.exitCode(-1);
 
-console.log(`Message ${repoName}`);
+console.log(`Installing required dependencies ${repoName}`);
 const installedDeps = runCommand(installDepsCommand);
-if(!installedDeps) proccess.exit(code,-1);
+if (!installedDeps) proccess.exitCode(-1);
 
 console.log("Message random");
-console.log(`cd ${repoName }&& npm start` );
+console.log(`cd ${repoName} & code .`);
