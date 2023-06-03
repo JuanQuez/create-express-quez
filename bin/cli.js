1aea72a6a3548ab4e2ec0e426ad98b70a87bf1f3
JuanQuez <fiestalatina.ft@gmail.com>
2023-06-03 16:30:21 -0500

    1.5.2

--- bin/cli.js
+++ bin/cli.js
@@ -16,7 +16,7 @@ const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/JuanQuez/create-express-quez ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
-console.log(`Installing your project with the name ${repoName}`);
+console.log(`\x1b[32mInstalling your project with the name ${repoName}\x1b[0m`);
 const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) process.exit(code,-1);
 
@@ -25,4 +25,3 @@ const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) proccess.exit(code,-1);
 
 console.log("Message random");
-console.log(`cd ${repoName}`);
