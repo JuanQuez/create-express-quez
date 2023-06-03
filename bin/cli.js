d213b8bf6bfa91cc9f345c383861238f9387dfae
JuanQuez <fiestalatina.ft@gmail.com>
2023-06-03 17:45:27 -0500

    1.5.3

--- bin/cli.js
+++ bin/cli.js
@@ -16,11 +16,32 @@ const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/JuanQuez/create-express-quez ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
-console.log(`\x1b[32mInstalling your project with the name ${repoName}\x1b[0m`);
+console.log("installing your project with the name");
 const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) process.exit(code,-1);
 
-console.log(`Installing required dependencies ${repoName}`);
+console.log(
+"              ,---------------------------,\n"+
+"              | |-----------------------| |\n"+
+"              | |                       | |\n"+
+"              | |      Installing       | |\n"+
+"              | |     Dependencies      | |\n"+
+`              | |${repoName            }| |\n`+
+"              | |                       | |\n"+
+"              | |_______________________| |\n"+
+"              |___________________________|\n"+
+"            ,---|_____     []     _______|------,\n"+
+"          /         |______________|           /|\n"+
+"        /___________________________________ /  | ___\n"+
+"        |                                   |   |    )\n"+
+"        |  _ _ _                 [-------]  |   |   (\n"+
+"        |  o o o                 [-------]  |  /    _)\n"+
+"        |__________________________________ |/     /  / \n"+
+"    /-------------------------------------/|      ( )/ \n"+
+"  /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ / \n"+
+"/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ / \n"+
+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+);
 const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) proccess.exit(code,-1);
 
